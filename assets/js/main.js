7e7e723e880b59a5f7b83398e9aa8b6724c16094
nahian28 <nahian.instructory@gmail.com>
2022-08-25 07:29:06 +0600

    Final COde

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // Project Popup
     $('.project-popup').magnificPopup({
         type:'image',
         gallery:{
@@ -6,11 +7,13 @@ $(document).ready(function() {
         }
     });
 
+    // Counter
     $('.counter').counterUp({
         delay: 10,
         time: 1000
     });
 
+    // Testimonials Carousel
     $(".testimonials").owlCarousel({
         items: 1,
         loop: true,
@@ -18,4 +21,35 @@ $(document).ready(function() {
         nav: false,
         dots: true
     });
+});
+
+$(window).load(function () {
+
+    // Sticky Header
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 1) {
+            $('.header').addClass("fixed");
+        } else {
+            $('.header').removeClass('fixed');
+        }
+    });
+
+    // Scroll Top Animation
+    $('#scrolltop').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, '1000')
+    });
+
+    // Scroll Top Fade
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 500) {
+            $('#scrolltop').css('opacity', '1');
+        } else {
+            $('#scrolltop').css('opacity', '0');
+        }
+    });
+
+    // Preloader
+    $('.preloader').fadeOut();
 });
